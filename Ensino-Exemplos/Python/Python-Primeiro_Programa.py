be78c39bf9336cacb7d5b301940bf42e105ccb5c
Wallace R. <wallacesantos12wr@gmail.com>
2025-05-09 21:50:41 -0300

    Atualização de conteúdos

---
+++ Ensino-Exemplos/Python/Python-Primeiro_Programa.py
@@ -0,0 +1,24 @@
+nome = input("Digite seu nome: ")
+print("Oi!", nome)
+
+
+x = 4
+y = 8
+adicao = x+y
+subtracao = x-y
+multiplicacao = x*y
+divisao = x/y
+outras = (x+y)/2
+
+print("Valor de X + Y: ", adicao)
+print("Valor de X + Y: ", subtracao)
+print("Valor de X + Y: ", multiplicacao)
+print("Valor de X + Y: ", divisao)
+print("Valor de X + Y: ", outras)
+
+lista_idade = [12, 23, 40]
+print("Idades: ", lista_idade)
+
+list_idade.append(36)
+lista_idade.append(1,55)
+lista_idade.remove(40)
